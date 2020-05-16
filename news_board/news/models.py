6af53ce5bc131dde163792fcf5fad775a34d1765
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2020-05-16 21:58:38 +0300

    Added Post and Comment models.

--- news_board/news/models.py
+++ news_board/news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.URLField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    upvotes_amount = models.PositiveIntegerField()
+    author_name = models.CharField(max_length=255)
+
+
+class Comment(models.Model):
+    author_name = models.CharField(max_length=255)
+    content = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
