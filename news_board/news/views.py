2783f423a09f8acde1d23d4352e28dc9a25a9120
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2020-05-16 23:48:29 +0300

    Added /posts url pattern.

--- news_board/news/views.py
+++ news_board/news/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Post
+
+
+class PostView(APIView):
+
+    def get(self, request):
+        posts = Post.objects.all()
+        return Response({"posts": posts})
