882db536a4726afe0d7b18d58918213efeb26824
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2020-05-17 11:17:10 +0300

    Changed serializers to model type.

--- news_board/news/serializers.py
+++ news_board/news/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 from .models import Post, Comment
 
-class PostSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=255)
-    link = serializers.URLField()
-    creation_date = serializers.DateTimeField()
-    upvotes_amount = serializers.IntegerField()
-    author_name = serializers.CharField(max_length=255)
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'link', 'creation_date', 'upvotes_amount', 'author_name']
 
 
-class CommentSerializer(serializers.Serializer):
-    author_name = serializers.CharField(max_length=255)
-    content = serializers.CharField()
-    creation_date = serializers.DateTimeField()
-    post_id = serializers.IntegerField()
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'author_name', 'content', 'creation_date', 'post_id']
