4bab7d7096fc1f57fa6743adcaa4dc9bdeb2b91d
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2020-05-18 02:04:26 +0300

    Changed secret key to env variable in prod.

--- news_board/news_board/settings.py
+++ news_board/news_board/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "w!$xj6o264t!0enx(g752uh@rdcu9lp5bol6w3dj^=91r&ar(m"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
