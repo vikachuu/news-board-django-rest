f3acd58e1f53845755d96c297abf84342573ac79
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2020-05-18 06:18:06 +0300

    Refactored to Generic API view.

--- news/views.py
+++ news/views.py
@@ -2,28 +2,25 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from django.http import Http404
+from rest_framework import mixins
+from rest_framework import generics
 
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
 
-class PostView(APIView):
+class PostView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     """Methods for /posts/ endpoint: create and get all.
     """
 
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
 class PostUpvote(APIView):
@@ -45,92 +42,61 @@ class PostUpvote(APIView):
             raise Http404
 
 
-class PostDetailView(APIView):
+class PostDetailView(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView,
+):
     """Methods for /posts/<:id>/ endpoint: get, put, delete.
     """
 
-    def get(self, request, pk: int):
-        try:
-            post = Post.objects.get(pk=pk)
-            serializer = PostSerializer(post)
-            return Response(serializer.data)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-        except Post.DoesNotExist:
-            raise Http404
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk: int):
-        try:
-            post = Post.objects.get(pk=pk)
-            serializer = PostSerializer(post, data=request.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        except Post.DoesNotExist:
-            raise Http404
-
-    def delete(self, request, pk: int):
-        try:
-            post = Post.objects.get(pk=pk)
-            post.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-        except Post.DoesNotExist:
-            raise Http404
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
-class CommentView(APIView):
+class CommentView(
+    mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView
+):
     """Methods for /comments/ endpoint: create and get all.
     """
 
-    def get(self, request):
-        comments = Comment.objects.all()
-        serializer = CommentSerializer(comments, many=True)
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
 
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request):
-        serializer = CommentSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class CommentDetailView(APIView):
+class CommentDetailView(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView,
+):
     """Methods for /posts/<:id>/ endpoint: get, put, delete.
     """
 
-    def get(self, request, pk: int):
-        try:
-            comment = Comment.objects.get(pk=pk)
-            serializer = CommentSerializer(comment)
-            return Response(serializer.data)
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
 
-        except Comment.DoesNotExist:
-            raise Http404
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk: int):
-        try:
-            comment = Comment.objects.get(pk=pk)
-            serializer = CommentSerializer(comment, data=request.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        except Comment.DoesNotExist:
-            raise Http404
-
-    def delete(self, request, pk: int):
-        try:
-            comment = Comment.objects.get(pk=pk)
-            comment.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-        except Comment.DoesNotExist:
-            raise Http404
+    def delete(self, request, *args, **kwargs):
+        return self.delete(request, *args, **kwargs)
